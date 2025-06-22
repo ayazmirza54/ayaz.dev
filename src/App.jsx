4ebf23dd0c6e960cb6878be5b8d08fc9b28be38e
Ayaz Mirza <ayazmirza54@gmail.com>
2025-06-22 11:09:49 +0530

    Donne

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { FileDown, Github } from 'lucide-react'
+import { FileDown, Github, Linkedin } from 'lucide-react'
 function App() {
 
     const handleDownload = () => {
@@ -30,7 +30,7 @@ function App() {
         </div>
           <div className="flex flex-col items-start nav">
             <div className="flex flex-col md:flex-row justify-between border border-white w-[100%] md:h-[100px] justify-center items-center">
-              <div className="text-white flex navbar text-center text-xl lg:text-2xl font-mono w-full md:w-1/3 md:h-[100%] justify-center items-center p-4 border-b md:border-b-0 md:border-r border-white">About me</div>
+              <div className="text-white flex navbar text-center text-xl lg:text-2xl font-mono w-full md:w-1/3 md:h-[100%] justify-center items-center p-4 border-b md:border-b-0 md:border-r border-white"><a href="https://linkedin.com/in/ayazmirza54" target='_blank' className='text-white flex justity-center items-center download decoration-none  text-center text-xl lg:text-2xl font-mono'>Linkedin&nbsp;<Linkedin></Linkedin></a></div>
               <div className="text-white flex navbar text-center text-xl lg:text-2xl font-mono w-full md:w-1/3 md:h-[100%] justify-center items-center p-4 border-b md:border-b-0 md:border-r border-white"><a onClick={handleDownload} className="text-white flex download decoration-none  text-center text-xl lg:text-2xl font-mono justity-center items-center">download resume &nbsp; <FileDown /></a></div>
               <div className="text-white flex navbar text-center text-xl lg:text-2xl font-mono w-full md:w-1/3 md:h-[100%] justify-center items-center p-4"><a href="https://github.com/ayazmirza54" target='_blank' className='text-white flex justity-center items-center download decoration-none  text-center text-xl lg:text-2xl font-mono'>Github&nbsp;<Github></Github></a></div>
             </div></div>
